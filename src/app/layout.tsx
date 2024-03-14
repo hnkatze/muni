06486e8a837a95e1da7ec05d503bad12170274f0
HnKatze <henriquezhector1@homtail.com>
2024-03-14 15:56:05 -0600

    Create a base configurations and componentes base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import { NavBar } from "@/components/Navbar";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +18,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="light">
+      <body className={inter.className}>
+      <NavBar/>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }

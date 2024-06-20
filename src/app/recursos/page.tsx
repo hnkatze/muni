8d29574db3cf48e552e801dec0085bfb2593caf2
katze <henriquezhector1@hotmail.com>
2024-06-20 13:12:30 -0600

    No comment

--- src/app/recursos/page.tsx
+++ src/app/recursos/page.tsx
@@ -1,39 +1,36 @@
+"use client";
 import CardH from "@/components/CardH";
+import { billetes, heroes } from "@/lib/utils";
 
-
-
-export default function Procer(){
-  const heroes = [
-    { id: 1, name: 'Dionisio de Herrera' },
-    { id: 2, name: 'Jose Trinidad Cabañas' },
-    { id: 3, name: 'Cacique Lempira' },
-    { id: 4, name: 'Gral. Francisco Morazan' },
-    { id: 5, name: 'Jose Trinidad Reyes' },
-    { id: 6, name: 'Jose Cecilio del Valle' },
-    { id: 7, name: 'Bandera Nacional' },
-    { id: 8, name: 'Venado Cola Blanca' },
-    { id: 9, name: 'Escudo Nacional' },
-    { id: 10, name: 'Guacamaya' },
-    { id: 11, name: 'Orquidea Brassavola' },
-    { id: 12, name: 'Pino' },
-    { id: 13, name: 'Guacamaya Animada' },
-    { id: 14, name: 'Peque. Guacamaya' },
-];
-
-
+export default function Procer() {
   return (
     <div className="flex flex-col h-full w-full justify-center items-center">
-    <h2 className="p-9 md:p-14 text-2xl md:text-5xl text-center uppercase underline">
+      <h2 className="p-9 md:p-14 text-2xl md:text-5xl text-center uppercase underline">
         Descargables De Proceres Y Simbolos Patrios
-    </h2>
-    <div className="w-full flex flex-wrap justify-center gap-9 ">
+      </h2>
+      <div className="w-full flex flex-wrap justify-center gap-9 ">
         {heroes.map((hero) => (
-            <div key={hero.id} className="w-[70%] h-auto lg:h-2/4 md:w-[300px] flex-shrink-0 justify-center items-center ">
-                <CardH srcs={`/proceres/Heroes/${hero.id}.jpg`} alt={hero.name} name={hero.name} />
-            </div>
+          <div
+            key={hero.name}
+            className="w-[70%] h-auto lg:h-2/4 md:w-[300px] flex-shrink-0 justify-center items-center "
+          >
+            <CardH srcs={hero.src} alt={hero.name} name={hero.name} />
+          </div>
         ))}
+      </div>
+      <h2 className="p-9 md:p-14 text-2xl md:text-5xl text-center uppercase underline">
+        Lempiras, Moneda Nacional
+      </h2>
+      <div className="w-full flex flex-wrap justify-center gap-9 ">
+        {billetes.map((hero) => (
+          <div
+            key={hero.name}
+            className="w-[70%] h-auto lg:h-2/4 md:w-[300px] flex-shrink-0 justify-center items-center "
+          >
+            <CardH srcs={hero.src} alt={hero.name} name={hero.name} />
+          </div>
+        ))}
+      </div>
     </div>
-</div>
-  )
-
+  );
 }
---
+++ src/config/firebase.ts
@@ -0,0 +1,20 @@
+// firebaseConfig.ts
+import { initializeApp, FirebaseOptions, FirebaseApp } from "firebase/app";
+import { getStorage, FirebaseStorage } from "firebase/storage";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+};
+
+let firebaseApp: FirebaseApp;
+let firebaseStorage: FirebaseStorage;
+
+firebaseApp = initializeApp(firebaseConfig);
+firebaseStorage = getStorage(firebaseApp);
+
+export { firebaseStorage };

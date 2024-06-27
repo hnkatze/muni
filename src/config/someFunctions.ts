f0116f0ffb7afc8a3b5373222de7e3ce2996e6fc
Camilo Henriquez <henriquezhector1@hotmail.com>
2024-06-26 18:25:33 -0600

    Solve issue

--- src/config/someFunctions.ts
+++ src/config/someFunctions.ts
@@ -1,24 +1,24 @@
 import { toast } from "react-toastify";
-import { createContact, getContactsPrisma } from "./crude";
+import { createContact } from "./crude";
 
-export async function  handleSubtmitContactForm(data: ContactData) {
-    try {
-    
-        await createContact(data).then(() => {
-            toast.success("Listo, Nos Pondremos En Contacto Contigo Pronto.");
-        }
-        );    
-    } catch (error) {
-        toast.error("Parece que hubo un error, por favor intenta de nuevo más tarde.");
-    }
+export async function handleSubtmitContactForm(data: ContactData) {
+  try {
+    await createContact(data).then(() => {
+      toast.success("Listo, Nos Pondremos En Contacto Contigo Pronto.");
+    });
+  } catch (error) {
+    toast.error(
+      "Parece que hubo un error, por favor intenta de nuevo más tarde."
+    );
+  }
 }
 
-export async function getContacts (): Promise<ContactDataWithId[]> {
-    try {
-       const contacts = await getContactsPrisma();
-       return contacts;
-    } catch (error) {
-        console.error("Error getting documents: ", error);
-        throw error;
-    }
+export async function getContacts(): Promise<ContactDataWithId[]> {
+  try {
+    const contacts = await getContacts();
+    return contacts;
+  } catch (error) {
+    console.error("Error getting documents: ", error);
+    throw error;
+  }
 }

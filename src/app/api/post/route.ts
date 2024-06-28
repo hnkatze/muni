99a80b144fb7135eaf7ed4b3d0eac15d0bb716c3
HnKatze <henriquezhector1@homtail.com>
2024-06-28 17:44:07 -0600

    Add Prisma for using a postgresql

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,32 +1,27 @@
-
 import prisma from "@/config/db";
-import fs from "fs-extra";
 import { NextRequest, NextResponse } from "next/server";
-import path from "path";
+
 
 export async function GET() {
   try {
-    const filePath = path.join(process.cwd(), "data", "post.json");
-    let existingData = [];
-    if (await fs.pathExists(filePath)) {
-      existingData = await fs.readJson(filePath);
-    }
+    const existingData = await prisma.iPost.findMany();
     return new Response(JSON.stringify(existingData));
   } catch (error) {
     return new Response("error reading data file");
   }
 }
+
 export async function POST(req: NextRequest) {
   if (req.method === "POST") {
     try {
-      const newData = await req.json();
-     const data = await prisma.iPost. create({
+      const newData:IPostId = await req.json();
+     const data = await prisma.iPost.create({
         data: {
           id: newData.id,
-          titulo: newData.title,
-          descripcion: newData.content,
-          enlace: newData.link,
-          zonas: newData.areas,
+          titulo: newData.titulo,
+          descripcion: newData.descripcion,
+          enlace: newData.enlace,
+          zonas: newData.zonas,
         },
       });
       return new NextResponse(JSON.stringify(data.id), { status: 201 });
@@ -35,27 +30,19 @@ export async function POST(req: NextRequest) {
       return new Response("Error writing data file: " + error, { status: 500 });
     }
   } else {
-    return new Response(`Method ${req.method} Not Allowed`, { status: 405 });
+    return new Response(`Method ${req.method} Not Allowed`, { status: 405 }); 
   }
 }
-\
+
 export async function DELETE(req: NextRequest) {
   if (req.method === "DELETE") {
     try {
       const newData = await req.json();
-      const filePath = path.join(process.cwd(), "data", "post.json");
-
-      let existingData = [];
-      if (await fs.pathExists(filePath)) {
-        existingData = await fs.readJson(filePath);
-      }
-
-      const newDataArray = existingData.filter(
-        (item: any) => item.id !== newData.id
-      );
-
-      await fs.writeJson(filePath, newDataArray, { spaces: 2 });
-
+      await prisma.iPost.delete({
+        where: {
+          id: newData.id,
+        },
+      });
       return new NextResponse(
         JSON.stringify({ message: "Data deleted successfully" }),
         { status: 200 }
